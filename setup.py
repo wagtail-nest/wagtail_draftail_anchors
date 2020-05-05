51d83cada69b8f14117438ece51c88c1db5a8496
jacobtoppm <jacobtm@torchbox.com>
2020-05-05 14:21:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wagtail_draftail_anchors", # Replace with your own username
+    name="wagtail_draftail_anchors",
     version="0.0.1",
     author="TODO",
     author_email="TODO",
     description="A Draftail extension to add anchor identifiers to rich text",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="TODO",
+    url="https://github.com/jacobtoppm/wagtail_draftail_anchors",
     packages=setuptools.find_packages(),
     install_requires=[
         'wagtail>=2.2',
-        'python-docx>=0.8.10'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",

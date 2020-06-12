6a45663d3b14dc2b7b608fd081ef4a8ba074ec11
jacobtoppm <jacobtm@torchbox.com>
2020-06-12 14:45:22 +0100

    Change anchor icon to svg for Wagtail 2.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wagtail_draftail_anchors",
-    version="0.0.5",
+    version="0.1.0",
     author="Jacob Topp-Mugglestone, Quoc Duan",
     author_email="jacobtm@torchbox.com",
     description="A Draftail extension to add anchor identifiers to rich text",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/jacobtoppm/wagtail_draftail_anchors",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["wagtail>=2.2",],
+    install_requires=["wagtail>2.9",],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
